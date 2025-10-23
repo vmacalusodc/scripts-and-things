ff223330c3df82bf98ff72565c0c67565d29908e
vmacalusodc <136122859+vmacalusodc@users.noreply.github.com>
2025-10-23 10:33:25 -0400

    Delete support_tools directory

--- support_tools/admin_FixStuckAutomox.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set FILENAME=%~n0
-set SCRIPTDIR=%~dp0
-set ADMIN=0
-if /I "%FILENAME:~0,5%"=="admin" set ADMIN=1
-
-echo Running "%SCRIPTDIR%ScriptsDir\RunScript.ps1" with %FILENAME% (Admin=%ADMIN%)
-powershell.exe -ExecutionPolicy Bypass -NoProfile -File "%SCRIPTDIR%ScriptsDir\RunScript.ps1" -Script "%FILENAME%" -Admin %ADMIN%
